635a9adfd12a09b5abcb7900f46a754f46fd9707
Lucien Albert <lucienalbert.pro@gmail.com>
2021-05-18 16:56:36 +0200

    add styles in item editor

--- src/components/ItemEditor.js
+++ src/components/ItemEditor.js
@@ -1,29 +1,54 @@
 import './ItemEditor.css'
-import {useContext} from 'react'
+import {useContext, useEffect, useState} from 'react'
 import {GlobalContext} from '../contexts/globalContext'
 import findNodeByID from "../helpers/findNodeByID";
 
 const ItemEditor = (props) => {
   const {state, dispatch} = useContext(GlobalContext)
-
   const currentNode = findNodeByID(state.schema.node, state.currentItemId)
 
-  const brToNl = str => str.replace(/<br\s*\/?>/mg, "\n")
-  const nlToBr = str => str.replace(/(?:\r\n|\r|\n)/g, '<br>')
+  const [localText, setLocalText] = useState(currentNode?.value)
 
-  const onTextAreaChange = (e) => {
-    if (!e.target) return
-    const value = nlToBr(e.target.value)
-    dispatch({type: 'SET_NODE_VALUE', payload: {id: currentNode.id, value}})
+  const onTextareaInput = (e) => {
+    dispatch({type: 'SET_NODE_VALUE', payload: {id: currentNode.id, value: e.target.innerHTML}})
   }
+
+  const applyStyle = style => e => {
+    e.preventDefault()
+    e.nativeEvent.preventDefault()
+    document.execCommand(style, false)
+  }
+
+  const onTextareaKeyDown = e => {
+    if (e.keyCode === 13) {
+      return false;
+    }
+  };
+
+  useEffect(() => {
+    setLocalText(currentNode?.value)
+  }, [currentNode?.id])
 
   return (
     <div className="ItemEditor-wrapper">
       <div className="ItemEditor">
-        {!!currentNode ? (
-          <textarea className="ItemEditor-textarea" value={brToNl(currentNode.value)} onChange={onTextAreaChange}/>
-        ) : (
+        {!currentNode ? (
           <div>Aucun élément n'est sélectionné.</div>
+        ) : (
+          <>
+            <header className="ItemEditor-header">
+              <button className="ItemEditor-style" onMouseDown={applyStyle('italic')}>Italic</button>
+              <button className="ItemEditor-style" onMouseDown={applyStyle('bold')}>Bold</button>
+              <button className="ItemEditor-style" onMouseDown={applyStyle('underline')}>Underline</button>
+            </header>
+            <div
+              className="ItemEditor-textarea"
+              contentEditable={true}
+              onInput={onTextareaInput}
+              onKeyDown={onTextareaKeyDown}
+              dangerouslySetInnerHTML={{__html: localText}}
+            />
+          </>
         )}
       </div>
     </div>
